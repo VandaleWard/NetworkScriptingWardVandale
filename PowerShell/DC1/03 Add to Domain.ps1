b0bc0a05622abfaabe498051021c724f0defdb67
Ward Vandale <vandale.ward@student.howest.be>
2021-09-22 10:10:18 +0200

    BUG Fixed reboot system

--- PowerShell/DC1/03 Add to Domain.ps1
+++ PowerShell/DC1/03 Add to Domain.ps1
@@ -2,4 +2,6 @@
 
 Install-windowsfeature -name AD-Domain-Services -IncludeManagementTools
 
-Install-ADDSForest -DomainName $DomainName -InstallDNS -restart
+Install-ADDSForest -DomainName $DomainName -InstallDNS
+
+Restart-Computer
