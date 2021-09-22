684fb8ac30b73e8a080f032ccb96226f1c6e83f2
Ward Vandale <vandale.ward@gmail.con>
2021-09-22 09:44:47 +0200

    Script Static IP

---
+++ PowerShell/DC1/Static IP.ps1
@@ -0,0 +1,23 @@
+﻿$IP = "192.168.1.2"
+$MaskBits = 24 # This means subnet mask = 255.255.255.0
+$Gateway = "192.168.1.1"
+$Dns1 = "172.20.0.2"
+$Dns2 = "172.20.0.3"
+$IPType = "IPv4"
+# Retrieve the network adapter that you want to configure
+$adapter = Get-NetAdapter -Physical | Where-Object {$_.PhysicalMediaType -match "802.3" -and $_.Status -eq "up"}s
+# Remove any existing IP, gateway from our ipv4 adapter
+If (($adapter | Get-NetIPConfiguration).IPv4Address.IPAddress) {
+ $adapter | Remove-NetIPAddress -AddressFamily $IPType -Confirm:$false
+}
+If (($adapter | Get-NetIPConfiguration).Ipv4DefaultGateway) {
+ $adapter | Remove-NetRoute -AddressFamily $IPType -Confirm:$false
+}
+ # Configure the IP address and default gateway
+$adapter | New-NetIPAddress `
+ -AddressFamily $IPType `
+ -IPAddress $IP `
+ -PrefixLength $MaskBits `
+ -DefaultGateway $Gateway
+# Configure the DNS client server IP addresses
+$adapter | Set-DnsClientServerAddress -ServerAddresses ($Dns1, $Dns2)
