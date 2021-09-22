abb78425a2a57cfe545e2eb165157ff9d118fec0
Ward Vandale <vandale.ward@student.howest.be>
2021-09-22 11:14:05 +0200

    BUG Fixed script

--- PowerShell/DC1/04 Reverse Lookup.ps1
+++ PowerShell/DC1/04 Reverse Lookup.ps1
@@ -1,8 +1,11 @@
 ﻿$NetworkID = "192.168.1.0/24"
 $ZoneFile = "1.168.192.in-addr.arpa.dns"
 
-$Dns1 = "172.20.0.2"
-$Dns2 = "172.20.0.3"
+$IP = "192.168.1.2"
+$MaskBits = 24 # This means subnet mask = 255.255.255.0
+$Gateway = "192.168.1.1"
+$Dns1 = "192.168.1.2"
+$Dns2 = "192.168.1.3"
 $IPType = "IPv4"
 # Retrieve the network adapter that you want to configure
 $adapter = Get-NetAdapter -Physical | Where-Object {$_.PhysicalMediaType -match "802.3" -and $_.Status -eq "up"}
@@ -13,8 +16,13 @@ If (($adapter | Get-NetIPConfiguration).IPv4Address.IPAddress) {
 If (($adapter | Get-NetIPConfiguration).Ipv4DefaultGateway) {
  $adapter | Remove-NetRoute -AddressFamily $IPType -Confirm:$false
 }
+ # Configure the IP address and default gateway
+$adapter | New-NetIPAddress `
+ -AddressFamily $IPType `
+ -IPAddress $IP `
+ -PrefixLength $MaskBits `
+ -DefaultGateway $Gateway
 # Configure the DNS client server IP addresses
 $adapter | Set-DnsClientServerAddress -ServerAddresses ($Dns1, $Dns2)
-
 
 Add-DnsServerPrimaryZone -NetworkID $NetworkID -ZoneFile $ZoneFile -DynamicUpdate None -PassThru
