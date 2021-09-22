5205b470cb3311940d03691c443ce80878b0a0a5
Ward Vandale <vandale.ward@gmail.con>
2021-09-22 11:43:06 +0200

    rmeoved another DNS Reverse Lookup

--- PowerShell/DC1/04 Reverse Lookup.ps1
+++ PowerShell/DC1/04 Reverse Lookup.ps1
@@ -25,7 +25,5 @@ $adapter | New-NetIPAddress `
 # Configure the DNS client server IP addresses
 $adapter | Set-DnsClientServerAddress -ServerAddresses ($Dns1, $Dns2)
 
-Add-DnsServerPrimaryZone -NetworkID $NetworkID -ZoneFile $ZoneFile -DynamicUpdate None -PassThru
-
 Add-DnsServerPrimaryZone -NetworkId 192.168.1.0/24 -ReplicationScope Domain
 Register-DnsClient
