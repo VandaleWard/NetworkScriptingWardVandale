b1990d2659733f3fededc38318fe4636662cb5c8
Ward Vandale <vandale.ward@gmail.con>
2021-09-27 15:30:43 +0200

    Added  DHCP Script

---
+++ PowerShell/DC1/07 DHCP.ps1
@@ -0,0 +1,15 @@
+﻿Install-WindowsFeature -Name 'DHCP' –IncludeManagementTools -restart
+
+
+$dnsArray = "192.168.1.2","192.168.1.3"
+$macPrinter = "b8-e9-37-3e-55-86"
+Add-DhcpServerInDC -DnsName "DC1.intranet.mijnschool.be" -IPAddress 192.168.1.1
+Add-DhcpServerV4Scope -Name "Kortrijk" -StartRange 192.168.1.0 -EndRange 192.168.1.255 -SubnetMask 255.255.255.0
+Set-DhcpServerV4OptionValue -DnsServer $dnsArray -Router 192.168.1.1
+Add-Dhcpserverv4ExclusionRange -ScopeId 192.168.1.0 -StartRange 192.168.1.1 -EndRange 192.168.1.10
+Add-DhcpServerv4Reservation -ScopeId 192.168.1.0 -IPAddress 192.168.1.11 -ClientId $macPrinter -Description "Reservation for Printer"
+
+Add-DhcpServerV4Scope -Name "Brugge" -StartRange 192.168.2.0 -EndRange 192.168.2.255 -SubnetMask 255.255.255.0
+Set-DhcpServerV4OptionValue -DnsServer $dnsArray -Router 192.168.2.1
+Add-Dhcpserverv4ExclusionRange -ScopeId 192.168.2.0 -StartRange 192.168.2.1 -EndRange 192.168.2.10
+Restart-service dhcpserver
