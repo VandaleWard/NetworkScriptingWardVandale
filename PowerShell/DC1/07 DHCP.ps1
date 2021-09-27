9305677b1b92ec27aeeed667e8045209493bb276
Ward Vandale <vandale.ward@student.howest.be>
2021-09-27 15:48:27 +0200

    Ficxed bad scripting kiddo

--- PowerShell/DC1/07 DHCP.ps1
+++ PowerShell/DC1/07 DHCP.ps1
@@ -1,15 +1,10 @@
-﻿Install-WindowsFeature -Name 'DHCP' –IncludeManagementTools -restart
+﻿Install-WindowsFeature -Name 'DHCP' –IncludeManagementTools -Restart
 
 
 $dnsArray = "192.168.1.2","192.168.1.3"
 $macPrinter = "b8-e9-37-3e-55-86"
-Add-DhcpServerInDC -DnsName "DC1.intranet.mijnschool.be" -IPAddress 192.168.1.1
-Add-DhcpServerV4Scope -Name "Kortrijk" -StartRange 192.168.1.0 -EndRange 192.168.1.255 -SubnetMask 255.255.255.0
-Set-DhcpServerV4OptionValue -DnsServer $dnsArray -Router 192.168.1.1
+Add-DhcpServerInDC -DnsName "DC1.intranet.mijnschool.be" -IPAddress 192.168.1.2
+Add-DhcpServerV4Scope -Name "Kortrijk" -StartRange 192.168.1.1 -EndRange 192.168.1.254 -SubnetMask 255.255.255.0
+Set-DhcpServerV4OptionValue -DnsServer $dnsArray -Router 192.168.1.1 -Force
 Add-Dhcpserverv4ExclusionRange -ScopeId 192.168.1.0 -StartRange 192.168.1.1 -EndRange 192.168.1.10
 Add-DhcpServerv4Reservation -ScopeId 192.168.1.0 -IPAddress 192.168.1.11 -ClientId $macPrinter -Description "Reservation for Printer"
-
-Add-DhcpServerV4Scope -Name "Brugge" -StartRange 192.168.2.0 -EndRange 192.168.2.255 -SubnetMask 255.255.255.0
-Set-DhcpServerV4OptionValue -DnsServer $dnsArray -Router 192.168.2.1
-Add-Dhcpserverv4ExclusionRange -ScopeId 192.168.2.0 -StartRange 192.168.2.1 -EndRange 192.168.2.10
-Restart-service dhcpserver
