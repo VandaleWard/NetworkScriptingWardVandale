9e2d11c474454a4ad9d87c4d11a678ac70ed39d7
Ward Vandale <vandale.ward@student.howest.be>
2021-10-06 19:06:25 +0200

    Finished Add Groups Script

---
+++ PowerShell/DC1/10 AddGroups.ps1
@@ -0,0 +1,23 @@
+﻿Import-Module ActiveDirectory
+$AddGroups = Import-Csv -Path "C:\Users\Administrator\Desktop\NetworkScriptingWardVandale\PowerShell\DC1\FILES\Groups.csv" -Delimiter ";"
+
+ForEach($Group In $AddGroups)
+{
+    $Name = $Group.Name
+    $DisplayName = $Group.DisplayName
+    $Path = $Group.Path
+    $GroupScope = $Group.GroupScope
+    $GroupCategory = $Group.GroupCategory
+    $Desciption = $Group.Description
+
+    Write-Host "Creating Group $Name" -ForegroundColor Green
+
+    $CreateGroup = New-ADGroup `
+        -Name $Name `
+        -DisplayName $DisplayName `
+        -Path $Path `
+        -GroupScope $GroupScope `
+        -GroupCategory $GroupCategory `
+        -Description $Desciption
+
+}
