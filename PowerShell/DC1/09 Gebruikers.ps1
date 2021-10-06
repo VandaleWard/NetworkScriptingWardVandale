a863bc69510cb96db7552d12f3b2332ca44553bc
Ward Vandale <vandale.ward@gmail.con>
2021-10-06 11:31:18 +0200

    AD ADD USER

---
+++ PowerShell/DC1/09 Gebruikers.ps1
@@ -0,0 +1,26 @@
+﻿Import-Module ActiveDirectory
+$ADUsers = Import-Csv C:\Users\Administrator\Downloads\UserAccounts.csv -Delimiter ";"
+foreach ($User in $ADUsers) {
+    $Name = $User.Name
+    $SamAccountName = $User.SamAccountName
+    $GivenName = $User.GivenName
+    $Surname = $User.Surname
+    $DisplayName = $User.DisplayName
+    $AccountPassword = $User.AccountPassword
+    $HomeDrive = $User.HomeDrive
+    $HomeDirectory = $User.HomeDirectory
+    $ScriptPath = $User.ScriptPath
+    $Path = $User.Path
+    New-ADUser `
+        -Name $DisplayName `
+        -SamAccountName $Name `
+        -GivenName $Description `
+        -Surname $Path `
+        -DisplayName $DisplayName `
+        -AccountPassword $AccountPassword `
+        -HomeDrive $HomeDrive `
+        -HomeDirectory $HomeDirectory `
+        -ScriptPath $ScriptPath `
+        -Path $Path
+    Write-Host "The User $Name is created." -ForegroundColor Cyan
+}
