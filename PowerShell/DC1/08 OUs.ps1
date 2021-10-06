9aa986b78a6eac6aecb1587148cdf32ddcf08a72
Ward Vandale <vandale.ward@gmail.con>
2021-10-06 11:10:45 +0200

    Added OUs

---
+++ PowerShell/DC1/08 OUs.ps1
@@ -0,0 +1,14 @@
+﻿Import-Module ActiveDirectory
+$ADOUs = Import-Csv C:\Users\Administrator\Downloads\OUs.csv -Delimiter ";"
+foreach ($OU in $ADOUs) {
+    $DisplayName = $OU.DisplayName
+    $Name = $OU.Name
+    $Description = $OU.Description
+    $Path = $OU.Path
+    New-ADOrganizationalUnit `
+        -DisplayName $DisplayName `
+        -Name $Name `
+        -Description $Description `
+        -Path $Path
+    Write-Host "The OU $Name is created." -ForegroundColor Cyan
+}
