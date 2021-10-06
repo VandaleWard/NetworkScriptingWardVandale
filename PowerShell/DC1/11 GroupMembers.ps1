605ae1e63521f4636517be154d2df23a4a0bc2c5
Ward Vandale <vandale.ward@student.howest.be>
2021-10-06 19:17:12 +0200

    Added script to Add members to groups

---
+++ PowerShell/DC1/11 GroupMembers.ps1
@@ -0,0 +1,14 @@
+﻿Import-Module ActiveDirectory
+$GroupMembers = Import-Csv C:\Users\Administrator\Desktop\NetworkScriptingWardVandale\PowerShell\DC1\FILES\GroupMembers.csv -Delimiter ";"
+
+ForEach($Mem In $GroupMembers)
+{
+    $Member = $Mem.Member
+    $Identity = $Mem.Identity
+
+    Write-Host "Adding $Member to $Identity" -ForegroundColor Green
+
+    $CreateGroup = Add-ADGroupMember `
+        -Identity $Identity `
+        -Members $Member
+}
