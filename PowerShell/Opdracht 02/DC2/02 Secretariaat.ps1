c4086c0c49633b4bf6728f1930b3b992ffd67b77
Ward Vandale <vandale.ward@student.howest.be>
2021-10-20 10:46:58 +0200

    Adding roaming profile to secretariaat

---
+++ PowerShell/Opdracht 02/DC2/02 Secretariaat.ps1
@@ -0,0 +1,6 @@
+﻿$users = Get-ADGroupMember -identity "secretariaat"
+
+foreach ($user in $users) { 
+    $SamAccountName = $user.SamAccountName
+    Set-ADUser -Identity $user -Profilepath "\\DC2\Profiles$\$SamAccountName"
+}
