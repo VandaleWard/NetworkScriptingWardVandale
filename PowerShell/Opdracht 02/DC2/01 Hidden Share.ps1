a37f8242eb8158712e1a542ebf0b44cd3d22432a
Ward Vandale <vandale.ward@student.howest.be>
2021-10-20 09:18:44 +0200

    Added Hidden profiles share script

---
+++ PowerShell/Opdracht 02/DC2/01 Hidden Share.ps1
@@ -0,0 +1,23 @@
+﻿$ComputerName = "DC2"
+
+ Invoke-Command -ComputerName $ComputerName -ScriptBlock {  # Invoke-Command is used on DC1 to input commands on the MS without connecting to it
+             $profilesFolder = "c:\profiles" # var
+             Write-Host "Creating Home Folder"
+             New-Item -Path $profilesFolder -type directory -Force # Creating the folder
+
+             Write-Host "Making home folder a share"
+             New-SmbShare -Name 'profiles$' -Path $profilesFolder -FullAccess "Everyone" # Sharing the folder
+
+             Write-Host "Folder creation complete"
+
+             # Changing the Permissions NTFS
+             Write-Host "ACL"
+             $acl = Get-Acl $profilesFolder
+             $acl.SetAccessRuleProtection($True, $False)
+             $acl.Access | % { $acl.RemoveAccessRule($_) } # I remove all security
+             $rule = New-Object System.Security.AccessControl.FileSystemAccessRule('administrators', 'FullControl', "ContainerInherit, objectInherit", "None", 'Allow') # I set my admin account as also having access
+             $acl.AddAccessRule($rule)
+             $rule = New-Object System.Security.AccessControl.FileSystemAccessRule('Authenticated Users', 'ReadandExecute', "None", "None", "Allow")
+             $acl.AddAccessRule($rule)
+             (Get-Item $profilesFolder).SetAccessControl($acl)
+         }
