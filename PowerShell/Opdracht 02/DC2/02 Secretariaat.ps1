65a366fe9c65070432a5c86c757a0af90e1dfcaf
Ward Vandale <ward.vandale@student.howest.be>
2021-11-04 12:38:28 +0100

    Forgot to add some comments

--- PowerShell/Opdracht 02/DC2/02 Secretariaat.ps1
+++ PowerShell/Opdracht 02/DC2/02 Secretariaat.ps1
@@ -1,6 +1,6 @@
-﻿$users = Get-ADGroupMember -identity "secretariaat"
+﻿$users = Get-ADGroupMember -identity "secretariaat" #Getting Users in group Secretariaat
 
 foreach ($user in $users) { 
-    $SamAccountName = $user.SamAccountName
-    Set-ADUser -Identity $user -Profilepath "\\DC2\Profiles$\$SamAccountName"
+    $SamAccountName = $user.SamAccountName #Getting the AccountName to make the Profile Path
+    Set-ADUser -Identity $user -Profilepath "\\DC2\Profiles$\$SamAccountName" #Adding the profile path to the roaming profile
 }
