b89812c983f2720a0bd9d197b3264bf5f66f8247
Ward Vandale <vandale.ward@gmail.con>
2021-12-22 10:01:10 +0100

    Finished customize_firstrun.ps1

--- Examen-WardVandale/customize_firstrun.ps1
+++ Examen-WardVandale/customize_firstrun.ps1
@@ -1,8 +1,30 @@
-﻿$fileexists = Test-Path -Path E:\reports\first-file.txt
+﻿# Saving the contents of firstrun.sh in a var
+$currFile = Get-ChildItem "C:\Users\vanda\Desktop\Network Scripting\NetworkScriptingWardVandale\Examen-WardVandale\firstrun.sh" | Get-Content
 
-If (Test-Path -Path C:\Users\vanda\Desktop\Network Scripting\NetworkScriptingWardVandale\Examen-WardVandale\firstrun.sh) {
-Write-Host "FILE EXISTS"
+# Making a var to save the new file content
+$newFile = @()
+ 
+# Watching if the current firstrun.sh has already been modified 
+if ($currFile -like "*#Start fallback preconfig*"){
+        Write-Host "The script firstrun.sh has already been modified"
 }
-Else {
-Write-Host "file does not exist, please make sure the file is in the same directory"
-}
+else{
+    for ($i = 0; $i -lt $currFile.Length; $i++) {
+        # Search the line rm -f /boot/firstrun.sh
+        if ($currFile[$i] -like "*rm -f /boot/firstrun.sh*") {
+            # Add the following commands to the new NetFileContent var
+            $newFile += "#Start fallback preconfig
+                                file=`"/etc/dhcpcd.conf`"
+                                sed -i 's/#profile static_eth0/profile static/' `$file
+                                sed -i 's/#static ip_address=192.168.1.23/static ip_address=192.168.168.168/' `$file
+                                line=``grep -n ' # fallback to static profile' `$file | awk -F: '{ print `$1}'``
+                                sed -i `"`$line,`$ s/#interface eth0/interface eth0/`" `$file
+                                sed -i 's/#fallback static_eth0/arping 192.168.66.6\nfallback static_eth0/' `$file
+                                #end fallback preconf"
+        }
+    # Add the new lines in front onf the rm -f /boot/firstrun.sh line
+    $newFile += $currFile[$i]
+    # Save the NewFileContent to the current file
+    $newFile | Out-File "C:\Users\vanda\Desktop\Network Scripting\NetworkScriptingWardVandale\Examen-WardVandale\firstrun.sh" }
+    
+ }
